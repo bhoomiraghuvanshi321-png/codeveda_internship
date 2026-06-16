b30cf50df0ec4120cb6a7dfa6c95cd134b737e22
Bhoomi Raghuvanshi <bhoomiraghuvanshi321@gmail.com>
2026-06-16 12:16:35 +0530

    Level 2 completed with DB indexing

--- task1-react/src/Button.js
+++
@@ -1,9 +0,0 @@
-function Button({ children, onClick }) {
-  return (
-    <button onClick={onClick} style={{ margin: '0 5px', padding: '5px 10px' }}>
-      {children}
-    </button>
-  );
-}
-
-export default Button;
