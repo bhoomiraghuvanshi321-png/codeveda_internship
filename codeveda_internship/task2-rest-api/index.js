8884d89c096737a9c459f9088d65f03aa7ed9ab9
Bhoomi Raghuvanshi <bhoomiraghuvanshi321@gmail.com>
2026-06-29 19:25:24 +0530

    final: fix CORS and remove server.js

--- codeveda_internship/task2-rest-api/index.js
+++ codeveda_internship/task2-rest-api/index.js
@@ -9,7 +9,7 @@ app.use(cors({
   origin: ['https://codeveda-internship.vercel.app', 'http://localhost:3000'],
   credentials: true,
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'x-auth-token']
 }));
 app.options('*', cors()); 
 app.use(express.json());
